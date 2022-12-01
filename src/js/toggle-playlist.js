5e6c2b9eb782c56457e8a41b3fd2e49acfba8449
Finn Stitcher <foundfootage@protonmail.com>
2022-11-30 16:55:20 -0800

    buttons

--- src/js/toggle-playlist.js
+++ src/js/toggle-playlist.js
@@ -12,17 +12,17 @@ function togglePlaylistHandler(e) {
         
         if (playlistBody.matches('.hidden')) {
             // toggle visibility
-            playlistBody.className = 'pl-3 py-2.5 divide-y space-y-1.5 block';
+            playlistBody.className = 'block';
 
             // switch arrow
             if (target.matches('.pl-title')) {
-                // if target has children (is the div), get the last child and change its text
+                // if target is the div, get the last child and change its text
                 target.lastElementChild.textContent = '▲';
             } else if (target.matches('.pl-title h3')) {
                 // if target is the h3 tag, get the sibling
                 target.nextElementSibling.textContent = '▲';
             } else if (target.matches('.pl-title p')) {
-                // if target was the arrow, get self
+                // if target is the arrow, get self
                 target.textContent = '▲';
             }
         } else if (playlistBody.matches('.block')) {
@@ -31,7 +31,7 @@ function togglePlaylistHandler(e) {
 
             // switch arrow
             if (target.matches('.pl-title')) {
-                // if target has children (is the div), get the last child and change its text
+                // if target is the div, get the last child and change its text
                 target.lastElementChild.textContent = '▼';
             } else if (target.matches('.pl-title h3')) {
                 // if target is the h3 tag, get the sibling
