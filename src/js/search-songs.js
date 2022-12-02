61cda2a6b38f567d88dcd6e7c82317eaa6dc2761
Finn Stitcher <foundfootage@protonmail.com>
2022-12-01 18:03:11 -0800

    select and deselect songs

--- src/js/search-songs.js
+++ src/js/search-songs.js
@@ -1,11 +1,27 @@
 const songContainerEl = document.getElementById('song-container');
 const songSearchInputEl = document.getElementById('song-search');
+const selectionContainerEl = document.getElementById('selected-songs');
+
+// used to check for duplicates
+const selectedSongs = [];
+
+// lets the user make searches
+songSearchInputEl.addEventListener('keyup', songSearchInputHandler);
+
+// double-clicking a song will add it to the selection list
+songContainerEl.addEventListener('dblclick', songSelectHandler);
+
+// double-clicking a selected song will remove it from that list
+selectionContainerEl.addEventListener('dblclick', songDeselectHandler);
 
 function songSearchInputHandler(event) {
     const {value} = event.target;
+
+    // regexp that excludes whitespace
+    const notWhitespaceRegex = /\S/
     
     // make sure there's actually text in the search bar
-    if (value) {
+    if (value && notWhitespaceRegex.test(value)) {
         searchSongs(value);
     }
 };
@@ -29,7 +45,7 @@ async function searchSongs(value) {
 function printSong(element) {
     const {title, artist, _id} = element;
 
-    const songEl = document.createElement('p');
+    const songEl = document.createElement('li');
     songEl.textContent = `${title} - ${artist}`;
     songEl.setAttribute('data-id', _id);
 
@@ -43,4 +59,32 @@ function printNoResultsMessage() {
     songContainerEl.appendChild(messageEl);
 };
 
-songSearchInputEl.addEventListener('keyup', songSearchInputHandler);
+function songSelectHandler(event) {
+    const {target} = event;
+
+    const isLi = target.matches('li');
+
+    if (isLi) {
+        // make sure there isn't an element in selectionContainerEl with this data-id property
+        const targetId = target.getAttribute('data-id');
+
+        if (selectedSongs.indexOf(targetId) !== -1) {
+            target.remove();
+            return;
+        }
+
+        selectedSongs.push(targetId);
+        // make a clone of this element in selectionContainerEl
+        selectionContainerEl.appendChild(target);
+    }
+};
+
+function songDeselectHandler(event) {
+    const {target} = event;
+    
+    const isLi = target.matches('li');
+
+    if (isLi) {
+        songContainerEl.appendChild(target);
+    }
+};
