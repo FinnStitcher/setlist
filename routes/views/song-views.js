2c64cc989bbd2615573fa61a82f05ae81ae202f9
Finn Stitcher <foundfootage@protonmail.com>
2023-01-29 22:59:06 -0800

    edit song page

--- routes/views/song-views.js
+++ routes/views/song-views.js
@@ -11,4 +11,15 @@ router.get('/add-song', (req, res) => {
     res.render('create-song', {loggedIn});
 });
 
+router.get('/edit-song', (req, res) => {
+    const {loggedIn} = req.session;
+
+    if (!loggedIn) {
+        res.render('auth-failed', {loggedIn});
+        return;
+    }
+
+    res.render('edit-song', {loggedIn});
+})
+
 module.exports = router;
