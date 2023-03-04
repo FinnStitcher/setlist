44f2bc2181d7de4cd3bc4bd480fdb1c575d7c2f4
Finn Stitcher <foundfootage@protonmail.com>
2023-03-04 15:49:09 -0800

    folder form made, not functional

--- routes/views/folder-views.js
+++ routes/views/folder-views.js
@@ -2,7 +2,7 @@ router = require('express').Router();
 const {User, Folder} = require('../../models');
 const {checkUserOwndership} = require('../../utils/utils.js');
 
-router.get('/manage-folders', async (req, res) => {
+router.get('/folders', async (req, res) => {
     const {loggedIn, user_id} = req.session;
 
     if (!loggedIn) {
@@ -26,6 +26,27 @@ router.get('/manage-folders', async (req, res) => {
     .catch(err => err);
 
     res.render('manage-folders', {userData, belongsToThisUser: true, loggedIn});
+});
+
+router.get('/new-folder', async (req, res) => {
+    const {loggedIn, user_id} = req.session;
+
+    if (!user_id) {
+        res.render('auth-failed', {loggedIn});
+        return;
+    }
+
+    // get this user's playlists
+    const userData = await User.findOne({
+        _id: user_id
+    })
+    .lean()
+    .select('playlists')
+    .populate('playlists')
+    .then(dbRes => dbRes)
+    .catch(err => err);
+
+    res.render('create-folder', {loggedIn, userData});
 });
 
 module.exports = router;
--- src/js/create-folder.js
+++ src/js/create-folder.js
@@ -1,1 +1,34 @@
-// going to make the "playlists" array on a user object actually refer to unsorted playlists, so when you sort a playlist, it'll be removed from that array
+const formEl = document.querySelector('#fl-form');
+const nameInputEl = document.querySelector('#fl-name');
+const selectedContainerEl = document.querySelector('#selected-pls');
+const searchResultsContainerEl = document.querySelector('#pl-search-results');
+
+// SUBMIT CODE
+// END SUBMIT CODE
+
+// FILTER CODE
+// END FILTER CODE
+
+// SORTABLE CODE
+Sortable.create(selectedContainerEl, {
+    group: {
+        name: 'playlists'
+    },
+    animation: 150,
+    ghostClass: 'opacity-0',
+    dragClass: 'bg-stone-300'
+});
+
+Sortable.create(searchResultsContainerEl, {
+    group: {
+        name: 'playlists'
+    },
+    sort: false,
+    animation: 150,
+    ghostClass: 'opacity-0',
+    dragClass: 'bg-stone-300'
+});
+// END SORTABLE CODE
+
+// MODAL CODE
+// END MODAL CODE
